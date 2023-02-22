e2f0aa63825e2530e2f4c670ea71e1a24f592fff
Mohammed Karchal <karchalmohammed@gmail.com>
2023-02-21 21:24:03 -0800

    A function that prints the n times table, starting with 0

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,72 +1,47 @@
 #include "main.h"
 
 /**
- * print_times_table(int x)- prints the n times table, starting with 0
+ * print_times_table - prints the n times table, starting with 0
  * @x: Is n times table
  *
  * Return: Nothing
  */
 void print_times_table(int x)
 {
-	int n, m, r, rr, rl, rm;
-	if (x <= 15 && x >= 0)
+	int n, m;
+
+	if (x > 15 || x < 0)
+		return;
+	for (n = 0; n <= x; n++)
 	{
-		for (n = 0; n <= x; n++)
+		for (m = 0; m <= x; m++)
 		{
-			for (m = 0; m <= x; m++)
+			int r = n * m;
+
+			if (m != 0)
 			{
-				r = n * m;
-				if (r > 99)
-				{
-					rl = r / 100;
-					rm = (r / 10) % 10;
-					rr = r % 10;
-					_putchar(rl + '0');
-					_putchar(rm + '0');
-					_putchar(rr + '0');
-					if (m != x)
-					{
-						_putchar(',');
-						_putchar(' ');
-					}
-				}
-				else if (r > 9)
-				{
-					rl = r / 10;
-					rr = r % 10;
-					_putchar(rl + '0');
-					_putchar(rr + '0');
-					if (r + n <= 99 && m != x)
-					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar(' ');
-					}
-					else if (m != x)
-					{
-						_putchar(',');
-						_putchar(' ');
-					}
-				}
-				else
-				{
-					_putchar(r + '0');
-					if (r + n <= 9 && m != x)
-					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(' ');
-					}
-					else if (m != x)
-					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar(' ');
-					}
-				}
+				_putchar(',');
+				_putchar(' ');
 			}
-			_putchar('\n');
+			if (r < 10)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(r + '0');
+			}
+			else if (r < 100)
+			{
+				_putchar(' ');
+				_putchar(r / 10 + '0');
+				_putchar(r % 10 + '0');
+			}
+			else
+			{
+				_putchar(r / 100 + '0');
+				_putchar((r / 10) % 10 + '0');
+				_putchar(r % 10 + '0');
+			}
 		}
+		_putchar('\n');
 	}
 }
