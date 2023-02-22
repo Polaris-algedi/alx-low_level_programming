22b85d4c7c2a1fbb6a69faf42f03275c10b12e38
Mohammed Karchal <karchalmohammed@gmail.com>
2023-02-21 22:38:35 -0800

    A program that computes and prints the sum of all the multiples of 3 or 5 below 1024 (excluded), followed by a new line

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -4,7 +4,8 @@
  * print_sum_multiples()- prints the sum of all the multiples
  * of 3 or 5 below 1024 (excluded)
  *
- * Return: 1 if @n is positive, 0 if @n is zero else -1
+ * Return: Nothing
+ *
  */
 void print_sum_multiples(void)
 {
@@ -16,4 +17,14 @@ void print_sum_multiples(void)
 			sum += l;
 	}
 	printf("%d\n", sum);
+}
+/**
+ * main- Entry point
+ * Description: Call the function print_sum_multiples()
+ * Return: 0
+ */
+int main(void)
+{
+	print_sum_multiples();
+	return (0);
 }
