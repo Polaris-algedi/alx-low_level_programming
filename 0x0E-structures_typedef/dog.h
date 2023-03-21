0e27ab7794c23ccd4b675f6be25aa61f3d45449f
Mohammed Karchal <karchalmohammed@gmail.com>
2023-03-20 17:16:14 -0700

    This commit adds a new struct definition for a dog. The struct contains three

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef _DOG_
+#define _DOG_
+
+
+/**
+ * struct dog - A struct that represents a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The name of the dog's owner.
+ *
+ * Description: This struct contains information about a dog, including its
+ * name, age, and owner's name.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif
