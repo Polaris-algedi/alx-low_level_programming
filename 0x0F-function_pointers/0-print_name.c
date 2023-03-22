1228223e1d4d7146c66a60ca9bf122428ec533a6
Mohammed Karchal <karchalmohammed@gmail.com>
2023-03-22 12:42:55 -0700

    Add a check for NULL arg for the 3 functions

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,4 @@
+#include <stddef.h>
 /**
  * print_name - prints a name
  * @name: pointer to the name string
@@ -6,5 +7,7 @@
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL || f == NULL)
+		return;
 	f(name);
 }
