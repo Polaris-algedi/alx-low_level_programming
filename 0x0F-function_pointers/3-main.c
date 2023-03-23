7a06a4fbc53dfcff4c45ebd7a35d8bf46150408b
Mohammed Karchal <karchalmohammed@gmail.com>
2023-03-22 18:55:59 -0700

    Fix exit() numbers

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -18,7 +18,7 @@ int main(int argc, char *argv[])
 	if (argc != 4)
 	{
 		printf("Error\n");
-		exit(1);
+		exit(98);
 	}
 
 	num1 = atoi(argv[1]);
@@ -27,14 +27,14 @@ int main(int argc, char *argv[])
 	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
-		exit(2);
+		exit(99);
 	}
 
 	if ((*op == '/' && num2 == 0) ||
 	    (*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
-		exit(3);
+		exit(100);
 	}
 
 	printf("%d\n", get_op_func(op)(num1, num2));
