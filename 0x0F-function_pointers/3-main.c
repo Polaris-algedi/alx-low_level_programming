b3ca1f01617b6b6a01f9b2bbd3e46bc8860ccb8e
Mohammed Karchal <karchalmohammed@gmail.com>
2023-03-22 18:49:51 -0700

    A program that performs simple operations

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+#include "3-calc.h"
+
+/**
+ * main - Performs simple operations
+ * @argc: The number of arguments passed to the program
+ * @argv: An array of pointers to the arguments passed to the program
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2;
+	char *op;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	op = argv[2];
+	if (get_op_func(op) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	if ((*op == '/' && num2 == 0) ||
+	    (*op == '%' && num2 == 0))
+	{
+		printf("Error\n");
+		exit(3);
+	}
+
+	printf("%d\n", get_op_func(op)(num1, num2));
+
+	return (0);
+}
+
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,59 @@
+/**
+ * op_add - Calculates the sum of two numbers
+ * @a: First operand
+ * @b: Second operand
+ *
+ * Return: the sum of a and b.
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Calculates the difference of two numbers.
+ * @a: First operand
+ * @b: Second operand
+ *
+ * Return: The difference of a and b.
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Calculates the product of two numbers.
+ * @a: First operand
+ * @b: Second operand
+ *
+ * Return: The product of a and b.
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Calculates the division of two numbers.
+ * @a: First operand
+ * @b: Second operand
+ *
+ * Return: The quotient of a and b.
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Calculates the remainder of the division of two numbers.
+ * @a: First operand
+ * @b: Second operand
+ *
+ * Return: The remainder of the division of a by b.
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
