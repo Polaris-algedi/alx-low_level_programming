a33286777025452c2eac2f85a6cb1e45da26a5b8
Mohammed Karchal <karchalmohammed@gmail.com>
2023-04-04 05:22:16 -0700

    Fix function get_nodeint_at_index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -14,10 +14,12 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i;
 
-	if (!head)
-		return (NULL);
 
 	for (i = 0; i < index; i++)
+	{
+		if (!head)
+			return (NULL);
 		head = head->next;
+	}
 	return (head);
 }
