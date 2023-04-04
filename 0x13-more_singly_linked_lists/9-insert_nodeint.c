ae20a8b028b622e30cf8060f3e2f46ebf3b683fe
Mohammed Karchal <karchalmohammed@gmail.com>
2023-04-04 05:40:49 -0700

    Return null when arg head = NULL and fix the pointer head when idx = 0

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,6 +16,17 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i;
 	listint_t *new, *temp;
 
+	new = (listint_t *)malloc(sizeof(listint_t));
+	if (!head || !new)
+		return (NULL);
+
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+
 	temp = *head;
 	for (i = 0; i < idx - 1; i++)
 	{
@@ -24,10 +35,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		temp = temp->next;
 	}
 
-
-	new = (listint_t *)malloc(sizeof(listint_t));
-	if (!new)
-		return (NULL);
 
 	new->n = n;
 	new->next = temp->next;
