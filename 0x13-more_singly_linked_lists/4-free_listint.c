07f24c3e7d9fff3baa2373827d5b7c0b42131ae1
Mohammed Karchal <karchalmohammed@gmail.com>
2023-04-04 05:05:06 -0700

    Fix free function in 4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -10,10 +10,12 @@ void free_listint(listint_t *head)
 {
 	listint_t *temp;
 
-	temp = head->next;
-	free(head);
-	head = temp;
-
 	if (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+
 		free_listint(head);
+	}
 }
