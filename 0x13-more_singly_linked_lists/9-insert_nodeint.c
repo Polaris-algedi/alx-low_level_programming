7913cb979575ebab55bd3379303c5b13edfd5301
Mohammed Karchal <karchalmohammed@gmail.com>
2023-04-04 13:01:37 -0700

    Function know can handle idx = 0 correctly

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,9 +17,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new, *temp;
 
 	new = (listint_t *)malloc(sizeof(listint_t));
-	if (!head || !new)
+	if (!head || !new || !(*head))
 		return (NULL);
 
+	new->n = n;
 	if (idx == 0)
 	{
 		new->next = *head;
@@ -35,8 +36,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		temp = temp->next;
 	}
 
-
-	new->n = n;
 	new->next = temp->next;
 	temp->next = new;
 
