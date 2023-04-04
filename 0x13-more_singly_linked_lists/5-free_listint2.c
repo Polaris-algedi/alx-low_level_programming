27f454d2ddfb8d6a5eaefbf232eacf07073742ef
Mohammed Karchal <karchalmohammed@gmail.com>
2023-04-04 05:07:46 -0700

    Fix free function in 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,10 +10,12 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	temp = (*head)->next;
-	free(*head);
-	*head = temp;
-
 	if (*head)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+
 		free_listint2(head);
+	}
 }
