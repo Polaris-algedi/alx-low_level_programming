078351ce05c47fb2131181e167aa7c3dfd32973c
Plaris-algedi <karchalmohammed@gmail.com>
2023-05-04 10:51:39 -0700

    Add a function that converts a binary number to an unsigned int using shift operator

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,11 +6,12 @@
  * there is one or more chars in the string b that is not 0 or 1.
  * b is NULL.
  */
-unsigned int binary_to_uint(const char *b) {
-    unsigned int result = 0;
-    
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0;
+
 	if (!b)
-		return 0;
+		return (0);
 	for (int i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] == '0')
@@ -18,7 +19,7 @@ unsigned int binary_to_uint(const char *b) {
 		else if (b[i] == '1')
 			result = (result << 1) + 1;
 		else
-			return 0;
+			return (0);
 	}
 	return (result);
 }
