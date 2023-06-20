2c0c6584d3b5e7bde013c9b5c0294a51eda7a479
Polaris-algedi <102882133+Polaris-algedi@users.noreply.github.com>
2023-06-20 02:36:38 +0100

    Update the link from master to main

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://github.com/Polaris-algedi/alx-low_level_programming/master/0x18-dynamic_libraries/win.so
+wget -P /tmp/ https://github.com/Polaris-algedi/alx-low_level_programming/main/0x18-dynamic_libraries/win.so
 export LD_PRELOAD=/tmp/win.so
