c0d0f183ca1b016a5f320d589c8c2a971f7bcd3f
Plaris-algedi <karchalmohammed@gmail.com>
2023-07-07 11:31:56 -0700

    Add a function that adds an element to the hash table

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,39 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - Adds an element to the hash table.
+ * @ht: The hash table you want to add or update the key/value to.
+ * @key: The key and cannot be an empty string.
+ * @value: The value associated with the key.
+ *
+ * value must be duplicated. value can be an empty string.
+ * In case of collision, add the new_node node at the beginning of the list.
+ *
+ * Return: 1 if it succeeded, 0 otherwise.
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new;
+	unsigned long int index;
+
+	if (!ht || !key || !value || key == "")
+		return (0);
+
+	index = key_index(key, ht->size);
+	/* Create the node */
+	new = malloc(sizeof(hash_node_t));
+	if (!new)
+	{
+		dprintf(2, "Error: Can't malloc\n");
+		return (0);
+	}
+	new->key = strdup(key); /* To free afterwards*/
+	new->value = strdup(value); /* To free afterwards*/
+	new->next = NULL;
+	/* Case of a Collision or no Collision */
+	if (ht->array[index] != NULL)
+		new->next = ht->array[index];
+	ht->array[index] = new;
+
+	return (1);
+}
