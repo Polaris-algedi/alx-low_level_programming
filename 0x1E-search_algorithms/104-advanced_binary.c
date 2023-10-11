8da9c536c3d5e0d56865ca2bca92686be299e971
Polaris-algedi <karchalmohammed@gmail.com>
2023-10-11 15:40:55 -0700

    Add a function that searches for a value in a sorted array of integers using the Binary search algorithm and return the index of the first value in the array (if this value appears more than once in the array)

---
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,59 @@
+#include "search_algos.h"
+
+/**
+ * advanced_binary_recursion - Recursively searches for a value in a sorted
+ * array of integers
+ * using an advanced binary search algorithm.
+ *
+ * @array: Pointer to the sorted array to search in.
+ * @low: The low index of the search range.
+ * @high: The high index of the search range.
+ * @value: The value to search for.
+ *
+ * Return: If the value is found, return the index of the first
+ * occurrence in the array; otherwise, return -1.
+ */
+int advanced_binary_recursion(int *array, int low, int high, int value)
+{
+	int i, mid, result;
+
+	printf("Searching in array: %d", array[low]);
+	for (i = low + 1; i <= high; i++)
+		printf(", %d", array[i]);
+	printf("\n");
+
+	mid = low + (high - low) / 2;
+
+	if (array[mid] == value && high - low < 2)
+		return (mid);
+
+	if (low == high)
+		return (-1);
+
+	if (array[mid] == value)
+		result = advanced_binary_recursion(array, low, mid, value);
+	else if (array[mid] < value)
+		result = advanced_binary_recursion(array, mid + 1, high, value);
+	else
+		result = advanced_binary_recursion(array, low, mid - 1, value);
+	return (result);
+}
+
+/**
+ * advanced_binary - Searches for a value in a sorted array of integers using
+ * an advanced binary search algorithm.
+ *
+ * @array: Pointer to the sorted array to search in.
+ * @size: Number of elements in the array.
+ * @value: The value to search for.
+ *
+ * Return: If the value is found, return the index of the first
+ * occurrence in the array; otherwise, return -1.
+ */
+int advanced_binary(int *array, size_t size, int value)
+{
+	if (array == NULL)
+		return (-1);
+
+	return (advanced_binary_recursion(array, 0, (int)(size - 1), value));
+}
