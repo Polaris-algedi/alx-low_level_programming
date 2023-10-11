a90ef200502f4d779061365f95afd2126b9c7aec
Polaris-algedi <karchalmohammed@gmail.com>
2023-10-11 15:57:50 -0700

    Add a a function that searches for a value in a sorted array of integers using the Jump search algorithm in a singly linked list

---
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -0,0 +1,47 @@
+#include "search_algos.h"
+
+/**
+ * jump_list - Searches for a value in a singly linked list of integers using
+ * the jump search algorithm.
+ *
+ * @list: Pointer to the head of the linked list.
+ * @size: Number of elements in the linked list.
+ * @value: The value to search for.
+ *
+ * Return: If the value is found, return a pointer to the node containing
+ * the value; otherwise, return NULL.
+ */
+listint_t *jump_list(listint_t *list, size_t size, int value)
+{
+	size_t step, i;
+	listint_t *current, *previous;
+
+	if (list == NULL)
+		return (NULL);
+
+	if (list->n == value)
+		return (list);
+
+	step = (int)sqrt((double)size);
+	current = list;
+	while (current->n < value && current->next != NULL)
+	{
+		previous = current;
+		for (i = 0; i < step && current->next != NULL; i++)
+			current = current->next;
+
+		printf("Value checked array[%ld] = [%d]\n",
+				current->index, current->n);
+	}
+	printf("Value found between indexes [%ld] and [%ld]\n",
+			previous->index, current->index);
+
+	for (current = previous; current != NULL; current = current->next)
+	{
+		printf("Value checked array[%ld] = [%d]\n",
+				current->index, current->n);
+		if (current->n == value)
+			return (current);
+	}
+	return (NULL);
+}
